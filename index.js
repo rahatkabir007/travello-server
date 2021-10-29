65d005184ccf9da407ed8709e07de7ff20f43516
rahatkabir007 <rahatkabir04@gmail.com>
2021-10-29 18:41:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5002;
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skjxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("server connected");
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Travelo Server Is Running")
+})
+
+app.listen(port, () => {
+    console.log("Travelo Server Is Running at Port", port);
+})
