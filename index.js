db6b21e493d1282ab56d3a1d1313eac597733b41
rahatkabir007 <rahatkabir04@gmail.com>
2021-10-30 22:07:48 +0600

    load data done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +17,32 @@ async function run() {
     try {
         await client.connect();
         console.log("server connected");
+
+        const database = client.db('travelo');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders');
+        
+        //get or load addServices
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.json(services);
+        })
+
+        //get selected service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        //posting addServices
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        })
     }
     finally {
         // await client.close()
